7fee64b38f66e0f77f03bf96a082ee367377fe8c
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2024-12-14 19:15:48 +0600

    update credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBfqTU2sSF8t6LvXXDZXFC1CeLnTIqMxlc",
-  authDomain: "talk-wave-829bf.firebaseapp.com",
-  projectId: "talk-wave-829bf",
-  storageBucket: "talk-wave-829bf.firebasestorage.app",
-  messagingSenderId: "773053148168",
-  appId: "1:773053148168:web:159bce18910888640f75bd",
-  measurementId: "G-YRVJ6Y1LLZ"
+  apiKey: "AIzaSyCg9R5qdzfu4qzJuJhR6JTt47ppkcqU3yA",
+  authDomain: "talk-wave-7b01a.firebaseapp.com",
+  projectId: "talk-wave-7b01a",
+  storageBucket: "talk-wave-7b01a.firebasestorage.app",
+  messagingSenderId: "940354716771",
+  appId: "1:940354716771:web:c9ca1f722133711d112456"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const auth = getAuth();
-export const storage = getStorage();
-export const db = getFirestore()
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
