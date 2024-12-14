a406594c1d1cfb22e88c5abea3613b3a556912ec
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2024-12-14 15:43:37 +0600

    primary setup complete

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,22 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "chat-ab746.firebaseapp.com",
-  projectId: "chat-ab746",
-  storageBucket: "chat-ab746.appspot.com",
-  messagingSenderId: "901216368405",
-  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+  apiKey: "AIzaSyBfqTU2sSF8t6LvXXDZXFC1CeLnTIqMxlc",
+  authDomain: "talk-wave-829bf.firebaseapp.com",
+  projectId: "talk-wave-829bf",
+  storageBucket: "talk-wave-829bf.firebasestorage.app",
+  messagingSenderId: "773053148168",
+  appId: "1:773053148168:web:939d7936451f0da50f75bd",
+  measurementId: "G-QLMVSGTM6N"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore()
